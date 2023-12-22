755dd776d872453413fbc07c5c37db77f98416ac
digushok <legokinaman@gmail.com>
2023-12-22 21:41:33 +0300

    1

---
+++ src/ru/mirea/eremin/task1/Factorial.java
@@ -0,0 +1,10 @@
+import java.math.BigInteger;
+public class Factorial {
+    public static BigInteger calculateFactorial(int number) {
+        if (number <= 1) {
+            return BigInteger.ONE;
+        } else {
+            return calculateFactorial(number - 1).multiply(BigInteger.valueOf(number));
+        }
+    }
+}
